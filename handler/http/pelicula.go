99139fd66d38bfd089ed7b0ff637c636753e0b2c
José Geovany Chan Cuevas <jogeochanc@gmail.com>
2019-10-13 00:10:38 -0500

    Agregando validadores en los metodos POST y PUT

--- handler/http/pelicula.go
+++ handler/http/pelicula.go
@@ -20,6 +20,7 @@ import (
 
 	"../../driver"
 	"github.com/go-chi/chi"
+	"github.com/thedevsaddam/govalidator"
 )
 
 // NewPeliculaHandler ...
@@ -48,6 +49,36 @@ func (p *Pelicula) Fetch(w http.ResponseWriter, r *http.Request) {
 }
 
 func (p *Pelicula) Create(w http.ResponseWriter, r *http.Request) { // CREAR UNA NUEVA PELI
+
+	//CREAMOS NUESTRAS REGLAS DE VALIDACION
+	rules := govalidator.MapData{
+		"nombre":   []string{"required", "between:1,100"},
+		"director": []string{"required", "between:1,100"},
+		"anio":     []string{"required", "digits:4", "numeric"},
+	}
+
+	//CREAMOS LOS MENSAJES EN CASO DE NO CUMPLIR CON ALGUNA REGLA
+	messages := govalidator.MapData{
+		"nombre":   []string{"required:No ingresó el nombre de la pelicula", "between:El nombre de la pelicula debe contener entre 1 a 100 caracteres"},
+		"director": []string{"required:No ingresó del director de la pelicula", "between:El nombre del director de la pelicula debe contener entre 1 a 100 caracteres"},
+		"anio":     []string{"required:No ingreso el Año de la pelicula", "digits:El año solo puede contener 4 digitos", "numeric:El año debe ser un valor numérico"},
+	}
+
+	opts := govalidator.Options{
+		Request:         r,        // El objeto con la peticion
+		Rules:           rules,    // Reglas
+		Messages:        messages, // La personalizacion de los mensajes en caso de no cumplir las reglas
+		RequiredDefault: true,     // Todos los campos deben de pasar las reglas
+	}
+	v := govalidator.New(opts) // CREAMOS EL VALIDADOR CON LAS OPCIONES
+	e := v.Validate()          // VALIDAMOS
+
+	if len(e) != 0 {
+		err_validate := map[string]interface{}{"errors": e}
+		respondwithJSON(w, http.StatusBadRequest, err_validate)
+		return
+	}
+
 	pelicula := models.Pelicula{} //CREAMOS UN NEW OBJ DESDE EL MAP DEL MODELO
 
 	r.ParseForm()                                        // CONVERTIMOS LA REQUEST A UN FORMULARIO
@@ -69,6 +100,34 @@ func (p *Pelicula) Create(w http.ResponseWriter, r *http.Request) { // CREAR UNA
 
 // ACTUALIZA LA PELICULA DESDE EL ID EN LA URL
 func (p *Pelicula) Update(w http.ResponseWriter, r *http.Request) {
+	//CREAMOS NUESTRAS REGLAS DE VALIDACION
+	rules := govalidator.MapData{
+		"nombre":   []string{"required", "between:1,100"},
+		"director": []string{"required", "between:1,100"},
+		"anio":     []string{"required", "digits:4", "numeric"},
+	}
+
+	//CREAMOS LOS MENSAJES EN CASO DE NO CUMPLIR CON ALGUNA REGLA
+	messages := govalidator.MapData{
+		"nombre":   []string{"required:No ingresó el nombre de la pelicula", "between:El nombre de la pelicula debe contener entre 1 a 100 caracteres"},
+		"director": []string{"required:No ingresó del director de la pelicula", "between:El nombre del director de la pelicula debe contener entre 1 a 100 caracteres"},
+		"anio":     []string{"required:No ingreso el Año de la pelicula", "digits:El año solo puede contener 4 digitos", "numeric:El año debe ser un valor numérico"},
+	}
+
+	opts := govalidator.Options{
+		Request:         r,        // El objeto con la peticion
+		Rules:           rules,    // Reglas
+		Messages:        messages, // La personalizacion de los mensajes en caso de no cumplir las reglas
+		RequiredDefault: true,     // Todos los campos deben de pasar las reglas
+	}
+	v := govalidator.New(opts) // CREAMOS EL VALIDADOR CON LAS OPCIONES
+	e := v.Validate()          // VALIDAMOS
+
+	if len(e) != 0 {
+		err_validate := map[string]interface{}{"errors": e}
+		respondwithJSON(w, http.StatusBadRequest, err_validate)
+		return
+	}
 	id, _ := strconv.Atoi(chi.URLParam(r, "id")) //SE OBTIENE EL ID DEL URL Y SE CONVIERTE
 	data := models.Pelicula{ID: int(id)}         // ASIGNAMIOS EL ID AL MODELO
 
